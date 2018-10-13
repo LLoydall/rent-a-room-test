f253ccce37c2c15f352dee00359772d4b4a56d24
lloydallcfm <l.loydall@cloudfmgroup.com>
2018-10-13 09:08:30 +0100

    teaser component

---
+++ app/components/Teaser/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Teaser
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class Teaser extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+Teaser.propTypes = {};
+
+export default Teaser;
---
+++ app/components/Teaser/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Teaser from '../index';
+
+describe('<Teaser />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
